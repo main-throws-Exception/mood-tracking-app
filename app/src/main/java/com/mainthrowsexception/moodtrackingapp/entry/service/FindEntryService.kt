10c57f64cc4210da63374de25ecf5008944deeaa
RealThomasMiles <Dmilya2000@gmail.com>
2021-04-26 22:17:28 +0300

    Updated EntryService

--- app/src/main/java/com/mainthrowsexception/moodtrackingapp/entry/service/FindEntryService.kt
+++ app/src/main/java/com/mainthrowsexception/moodtrackingapp/entry/service/FindEntryService.kt
@@ -2,14 +2,14 @@ package com.mainthrowsexception.moodtrackingapp.entry.service
 
 import com.mainthrowsexception.moodtrackingapp.api.Api
 import com.mainthrowsexception.moodtrackingapp.api.dto.EntryDto
-import com.mainthrowsexception.moodtrackingapp.entry.model.Entry
-import com.mainthrowsexception.moodtrackingapp.entry.model.EntryId
+import com.mainthrowsexception.moodtrackingapp.entry.model.*
 import com.mainthrowsexception.moodtrackingapp.entry.repo.EntryRepo
 import com.mainthrowsexception.moodtrackingapp.util.Generator
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.withContext
 import java.util.*
+import kotlin.collections.ArrayList
 
 class FindEntryService (
     private val entryRepo: EntryRepo,
@@ -20,7 +20,7 @@ class FindEntryService (
     suspend fun find(id: EntryId): ServiceResult {
         return withContext(ioDispatcher) {
             try {
-                entryRepo.findById(id)?.let { return@withContext ServiceResult.Success(it) }
+                entryRepo.findById(id)?.let { return@withContext ServiceResult.Success(listOf(it)) }
                     ?: return@withContext fetchFromServer(id)
             } catch (t: Throwable) {
                 return@withContext ServiceResult.Failure
@@ -28,17 +28,60 @@ class FindEntryService (
         }
     }
 
+    suspend fun findByUserIdAndCreatedBetween(userId: UserId, createdLowerBound: Long, createdUpperBound: Long): ServiceResult {
+        return withContext(ioDispatcher) {
+            try {
+                val entriesList = entryRepo.findByUserIdAndCreatedBetween(userId, createdLowerBound, createdUpperBound)
+                if (entriesList.isEmpty()) {
+                    return@withContext ServiceResult.Success(entriesList)
+                } else {
+                    return@withContext fetchFromServerByUserIdAndCreatedBetween(userId, createdLowerBound, createdUpperBound)
+                }
+            } catch (t: Throwable) {
+                return@withContext ServiceResult.Failure
+            }
+        }
+    }
+
+    private suspend fun fetchFromServerByUserIdAndCreatedBetween(userId: UserId, createdLowerBound: Long, createdUpperBound: Long): ServiceResult {
+        delay(generator.timeoutMillis(3)) // emulating http request to server
+        val entriesDtoList = api.fetchEntriesByUserIdAndCreatedBetween(userId, createdLowerBound, createdUpperBound) ?: return ServiceResult.NotFound
+//        val tagsDtoList = api.fetchTagsByEntryId(id)
+//        val tagsList = tagsDtoListToModel(tagsDtoList)
+        val entriesList: MutableList<Entry> = ArrayList()
+        entriesDtoList.forEach { entryDto -> entriesList.add(entryRepo.add(entryDto.toModel())) }
+        return ServiceResult.Success(entriesList)
+    }
+
     private suspend fun fetchFromServer(id: EntryId): ServiceResult {
         delay(generator.timeoutMillis(3)) // emulating http request to server
-        val entryDto = api.fetchEntry(id) ?: return ServiceResult.NotFound
+        val entryDto = api.fetchEntryById(id) ?: return ServiceResult.NotFound
+//        val tagsDtoList = api.fetchTagsByEntryId(id)
+//        val tagsList = tagsDtoListToModel(tagsDtoList)
         val entry = entryRepo.add(entryDto.toModel())
-        return ServiceResult.Success(entry)
+        return ServiceResult.Success(listOf(entry))
     }
+
+//    private fun tagsDtoListToModel(tagsDtoList: List<TagDto>): List<Tag> {
+//        val tagsList: MutableList<Tag> = ArrayList()
+//
+//        for (i in 0..10) {
+//            tagsList.add(tagsDtoList[i].toModel())
+//        }
+//
+//        return tagsList
+//    }
+
+//    private fun TagDto.toModel(): Tag = Tag(
+//        TagId (id),
+//        name
+//    )
 
     private fun EntryDto.toModel(): Entry = Entry (
         EntryId (id),
+        UserId (userId),
+        note,
         mood,
-        tags,
         created,
         System.currentTimeMillis()
     )
--- app/src/main/java/com/mainthrowsexception/moodtrackingapp/entry/service/ServiceResult.kt
+++ app/src/main/java/com/mainthrowsexception/moodtrackingapp/entry/service/ServiceResult.kt
@@ -3,7 +3,7 @@ package com.mainthrowsexception.moodtrackingapp.entry.service
 import com.mainthrowsexception.moodtrackingapp.entry.model.Entry
 
 sealed class ServiceResult {
-    data class Success(val entry: Entry) : ServiceResult()
+    data class Success(val entry: List<Entry>) : ServiceResult()
     object Failure : ServiceResult()
     object NotFound : ServiceResult()
 }
