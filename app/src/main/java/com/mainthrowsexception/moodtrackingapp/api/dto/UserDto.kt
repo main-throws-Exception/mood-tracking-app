edf275b7f28195e550ba0975d3aa6fabf0954d90
RealThomasMiles <Dmilya2000@gmail.com>
2021-04-26 22:16:46 +0300

    Added User and UserDto

---
+++ app/src/main/java/com/mainthrowsexception/moodtrackingapp/api/dto/UserDto.kt
@@ -0,0 +1,8 @@
+package com.mainthrowsexception.moodtrackingapp.api.dto
+
+data class UserDto (
+    val id: Long,
+    val name: String,
+    val email: String,
+    val password: String
+)
---
+++ app/src/main/java/com/mainthrowsexception/moodtrackingapp/entry/model/UserId.kt
@@ -0,0 +1,3 @@
+package com.mainthrowsexception.moodtrackingapp.entry.model
+
+inline class UserId(val value: Long)
