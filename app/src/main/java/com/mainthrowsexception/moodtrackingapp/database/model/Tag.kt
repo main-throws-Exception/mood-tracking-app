ae32573b55e131ab082ecdb02438ff2cfd577fba
RealThomasMiles <Dmilya2000@gmail.com>
2021-05-02 14:39:55 +0300

    Updated tag model

--- app/src/main/java/com/mainthrowsexception/moodtrackingapp/database/model/Tag.kt
+++ app/src/main/java/com/mainthrowsexception/moodtrackingapp/database/model/Tag.kt
@@ -4,7 +4,6 @@ import com.google.firebase.database.PropertyName
 
 data class Tag (
     val uid: String = "",
-    @PropertyName("user_id")
     val userId: String = "",
     val name: String = ""
 )
