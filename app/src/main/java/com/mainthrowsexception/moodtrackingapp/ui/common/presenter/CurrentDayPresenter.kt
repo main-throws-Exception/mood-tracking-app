1375f7963bea5314506a06dd5e26f0771145209f
Alyona Zakharova <alenazakharovaspb@yandex.ru>
2021-05-03 21:45:15 +0300

    retrieve only today's data

--- app/src/main/java/com/mainthrowsexception/moodtrackingapp/ui/common/presenter/CurrentDayPresenter.kt
+++ app/src/main/java/com/mainthrowsexception/moodtrackingapp/ui/common/presenter/CurrentDayPresenter.kt
@@ -11,16 +11,31 @@ import com.google.firebase.ktx.Firebase
 import com.mainthrowsexception.moodtrackingapp.database.model.Entry
 import com.mainthrowsexception.moodtrackingapp.ui.common.contract.CurrentDayContract
 import com.mainthrowsexception.moodtrackingapp.ui.currentday.EntriesAdapter
+import java.time.Instant
+import java.time.LocalDateTime
+import java.time.ZonedDateTime
 
 class CurrentDayPresenter(private val view: CurrentDayContract.View,
                           private val recyclerView: RecyclerView) : CurrentDayContract.Presenter {
 
     override fun getEntries() {
         Log.i("CurrentDayPresenter", "getEntries() called")
+
+        val nowZoned: ZonedDateTime = ZonedDateTime.now()
+        val midnight: Long = nowZoned.toLocalDate().atStartOfDay(nowZoned.zone).toInstant().toEpochMilli()
+//        Log.i("MIDNIGHT", midnight.toString())
+
+        val now = System.currentTimeMillis()
+//        Log.i("NOW", now.toString())
+
         val userId = FirebaseAuth.getInstance().currentUser!!.uid
         val databaseRef = Firebase.database("https://goodmood-c69a1-default-rtdb.europe-west1.firebasedatabase.app/").reference
             .child("entries")
             .child(userId)
+            .orderByChild("created")
+            .startAt(midnight.toDouble())
+            .endAt(now.toDouble())
+
         val entries = ArrayList<Entry>()
 
         databaseRef.addValueEventListener(object: ValueEventListener {
