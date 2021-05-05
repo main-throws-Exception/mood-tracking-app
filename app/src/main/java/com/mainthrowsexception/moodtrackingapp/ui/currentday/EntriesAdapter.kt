44e6a109dd2c164ac29fcb374bb0cb82fa698cf2
Alyona Zakharova <alenazakharovaspb@yandex.ru>
2021-05-05 12:18:23 +0300

    delete selected entry from the database

--- app/src/main/java/com/mainthrowsexception/moodtrackingapp/ui/currentday/EntriesAdapter.kt
+++ app/src/main/java/com/mainthrowsexception/moodtrackingapp/ui/currentday/EntriesAdapter.kt
@@ -1,23 +1,34 @@
 package com.mainthrowsexception.moodtrackingapp.ui.currentday
 
 import android.annotation.SuppressLint
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.PopupMenu
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.widget.AppCompatButton
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.chip.ChipGroup
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 import com.mainthrowsexception.moodtrackingapp.R
 import com.mainthrowsexception.moodtrackingapp.database.model.Entry
 import java.util.*
 
 class EntriesAdapter(private val entries: MutableList<Entry>) : RecyclerView.Adapter<EntriesAdapter.EntryViewHolder>() {
 
+//    наверное, это должно быть не тут...)))
+    private val userId = FirebaseAuth.getInstance().currentUser!!.uid
+    private val databaseRef = Firebase.database("https://goodmood-c69a1-default-rtdb.europe-west1.firebasedatabase.app/").reference
+    val query = databaseRef.child("entries")
+        .child(userId)
+        .orderByChild("created")
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): EntryViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.entry, parent, false)
@@ -45,16 +56,19 @@ class EntriesAdapter(private val entries: MutableList<Entry>) : RecyclerView.Ada
         }
 
         private fun showPopup(view: View) {
+            val selectedEntry = entries[adapterPosition]
+
             val popupMenu = PopupMenu(view.context, view)
             popupMenu.inflate(R.menu.popup_menu)
             popupMenu.setOnMenuItemClickListener {
                 when(it.itemId) {
                     R.id.popup__edit -> {
-                        //
+                        Log.i("RECYCLER VIEW", "Edit clicked on " + selectedEntry.uid)
                         true
                     }
                     R.id.popup__delete -> {
-                        //
+                        Log.i("RECYCLER VIEW", "Delete clicked on " + selectedEntry.uid)
+                        databaseRef.child("entries").child(userId).child(selectedEntry.uid).removeValue()
                         true
                     }
                     else -> true
