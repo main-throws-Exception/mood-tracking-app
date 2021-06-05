d0f6de7bd72db00037700d0c33680f7924e23fd2
Fedorov_Danila <fedorov.dv@edu.spbstu.ru>
2021-06-05 17:45:44 +0300

    Removed depreceted getColor method

--- app/src/main/java/com/mainthrowsexception/moodtrackingapp/ui/settings/ColorSettingsPreferenceFragment.kt
+++ app/src/main/java/com/mainthrowsexception/moodtrackingapp/ui/settings/ColorSettingsPreferenceFragment.kt
@@ -2,6 +2,7 @@ package com.mainthrowsexception.moodtrackingapp.ui.settings
 
 import android.os.Bundle
 import android.view.View
+import androidx.core.content.ContextCompat
 import androidx.preference.Preference
 import com.mainthrowsexception.moodtrackingapp.R
 import com.mainthrowsexception.moodtrackingapp.ui.common.base.BasePreferenceFragment
@@ -22,11 +23,11 @@ class ColorSettingsPreferenceFragment : BasePreferenceFragment(), Preference.OnP
         when (preference?.key) {
             getString(R.string.reset_colors_key) -> {
                 resetColors()
-                resetPreference(findPreference<ColorPickerPreference>("very_bad_color_preference_key"))
-                resetPreference(findPreference<ColorPickerPreference>("bad_color_preference_key"))
-                resetPreference(findPreference<ColorPickerPreference>("neutral_color_preference_key"))
-                resetPreference(findPreference<ColorPickerPreference>("good_color_preference_key"))
-                resetPreference(findPreference<ColorPickerPreference>("very_good_color_preference_key"))
+                resetPreference(findPreference<ColorPickerPreference>(getString(R.string.very_bad_color_preference_key)))
+                resetPreference(findPreference<ColorPickerPreference>(getString(R.string.bad_color_preference_key)))
+                resetPreference(findPreference<ColorPickerPreference>(getString(R.string.neutral_color_preference_key)))
+                resetPreference(findPreference<ColorPickerPreference>(getString(R.string.good_color_preference_key)))
+                resetPreference(findPreference<ColorPickerPreference>(getString(R.string.very_good_color_preference_key)))
             }
         }
         return true
@@ -35,11 +36,11 @@ class ColorSettingsPreferenceFragment : BasePreferenceFragment(), Preference.OnP
     fun resetColors() {
         val sp = preferenceManager.sharedPreferences
 
-        sp.edit().putInt("very_bad_color_preference_key", resources.getColor(R.color.very_bad_mood))
-            .putInt("bad_color_preference_key", resources.getColor(R.color.bad_mood))
-            .putInt("neutral_color_preference_key", resources.getColor(R.color.neutral_mood))
-            .putInt("good_color_preference_key", resources.getColor(R.color.good_mood))
-            .putInt("very_good_color_preference_key", resources.getColor(R.color.very_good_mood))
+        sp.edit().putInt(getString(R.string.very_bad_color_preference_key), ContextCompat.getColor(requireContext(), R.color.very_bad_mood))
+            .putInt(getString(R.string.bad_color_preference_key), ContextCompat.getColor(requireContext(), R.color.bad_mood))
+            .putInt(getString(R.string.neutral_color_preference_key), ContextCompat.getColor(requireContext(), R.color.neutral_mood))
+            .putInt(getString(R.string.good_color_preference_key), ContextCompat.getColor(requireContext(), R.color.good_mood))
+            .putInt(getString(R.string.very_good_color_preference_key), ContextCompat.getColor(requireContext(), R.color.very_good_mood))
             .apply()
     }
 
